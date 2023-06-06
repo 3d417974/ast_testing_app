fde04c056c1228aab3fecc9a905cbaa82bcacea0
Maksym Lukianenko <mlukianenko@mindk.com>
2023-06-06 21:22:26 +0300

    init

---
+++ front/src/RegistrationPage.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { Typography, TextField, Button, Grid, Paper, Box } from '@mui/material';
+
+const RegistrationPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Perform any necessary validation on the input fields here
+
+        // Create the registration data object
+        const registrationData = {
+            email,
+            password,
+            name,
+            lastName,
+        };
+
+        try {
+            // Make an API call to the backend to register the user
+            const response = await fetch('http://localhost:3001/users/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(registrationData),
+            });
+
+            if (response.ok) {
+                // Registration successful, handle the success case
+                console.log('Registration successful');
+            } else {
+                // Registration failed, handle the error case
+                console.log('Registration failed');
+            }
+        } catch (error) {
+            console.error('Error registering user:', error);
+        }
+
+        // Clear the form fields
+        setEmail('');
+        setPassword('');
+        setName('');
+        setLastName('');
+    };
+
+    return (
+        <Grid container justifyContent="center">
+            <Grid item xs={12} sm={8} md={6}>
+                <Paper elevation={3} sx={{ p: 4 }}>
+                    <Box sx={{ mb: 2 }}>
+                        <Typography variant="h4">Registration</Typography>
+                    </Box>
+
+                    <form onSubmit={handleSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Email"
+                                    fullWidth
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Password"
+                                    fullWidth
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="First Name"
+                                    fullWidth
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Last Name"
+                                    fullWidth
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button variant="contained" type="submit" fullWidth>
+                                    Register
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default RegistrationPage;
